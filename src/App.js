aac620fe20a822c944d2a4caf23a7afce7ef423c
nnishant.303 <paliwalnishant0@gmail.com>
2025-07-11 11:32:23 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [name , set_name] = useState('');
+  const [greet , set_greet] = useState(false);
+
+  function change_name(e){
+    set_name(e.target.value);
+  }
+
+  function greet_change(){
+    if(name.trim() !== ""){
+      set_greet(true);
+    };
+  };
+
+  function reset(){
+    set_greet(false);
+    set_name('');
+  }
+
+
+  return(
+    <div className='App'>
+    <h1>Greet Message App</h1>
+
+    <input
+    type='text'
+    placeholder='Enter Your Name'
+    value={name}
+    onChange={change_name}
+    />
+    <br/>
+
+    <button onClick={greet_change}>Greet Message</button>
+    <button onClick={reset}>Reset</button>
+    
+    {greet && <h2>Hello, {name} !</h2>}
+    
     </div>
-  );
+  
+  )
 }
 
 export default App;
